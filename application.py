4a0f9c6054a7d585caa48c56dd06f0242a8cfda7
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-10-25 14:59:48 -0400

    display query all results and update data naming

--- application.py
+++ application.py
@@ -6,18 +6,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
 db = SQLAlchemy(app)
 
-class Song(db.Model):
+class Setlist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    description = db.Column(db.String(120))
+    songs = db.Column(db.String(120))
 
     def __repr__(self):
-        return f"{self.name} - {self.description}"
+        return f"{self.name} - {self.songs}"
 
 @app.route('/')
 def index():
     return 'Index Route'
 
-@app.route('/songs')
-def get_songs():
-    return {"songs": "songs data"}
+@app.route('/setlists')
+def get_setlists():
+    setlists = Setlist.query.all()
+
+    output = []
+    for setlist in setlists:
+        setlist_data = {'name': setlist.name, 'songs': setlist.songs}
+
+        output.append(setlist_data)
+
+    return {"setlists": setlist_data}
